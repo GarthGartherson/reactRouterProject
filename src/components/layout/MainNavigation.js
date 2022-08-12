40609ea4755868788e33e7ae2bffdf0344158a01
Garrett <garrettgreeves@gmail.com>
2022-08-11 19:57:13 -0500

    Made a quote library connected to firebase

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,30 @@
+import { Link, NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <nav className={classes.nav}>
+        <h1>
+          <Link to="/">Great Quotes</Link>
+        </h1>
+      </nav>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/quotes">
+              All Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/new-quote">
+              Add a Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
